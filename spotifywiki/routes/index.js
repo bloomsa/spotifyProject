1613c0acdfbf531004149c0a067629727d0aebdd
Sam Bloomquist <bloom246@umn.edu>
2018-07-19 18:43:41 -0500

    working on user authentication

--- spotifywiki/routes/index.js
+++ spotifywiki/routes/index.js
@@ -1,9 +1,123 @@
 var express = require('express');
+var request = require('request'); // "Request" library
 var router = express.Router();
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+//  most of this was copied from the example, not working yet. gonna be messing 
+//  around with it
+
+// update: not crashing, gonna try redirecting to display user's data
+
+var client_id = 'c40a9f6742b549358d9433068af6a76e'; // Your client id
+var client_secret = '2adc823da40b4445b01268a6e339be0a'; // Your secret
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+
+//  generate random string function used from publically available
+//  source code of spotify web-api-auth-examples in the authorization
+//  code's app.js file 
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'spotify-wiki web app' });
 });
+
+router.get('/login', function(req, res, next) {
+  // i think generating a cookie to be stored with the site for future log ins
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  var scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+  // this causes an error for some reason
+  // res.render('login');
+});
+
+router.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+        //the following is what displays the user info on the console
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+  res.render('login', { title: 'display_name' });
+ });
 
 module.exports = router;
