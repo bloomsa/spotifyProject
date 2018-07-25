fb61e9c308b6970243184394821187780c2419dd
bloomsa <bloom246@umn.edu>
2018-07-25 17:19:07 -0500

    testing git settings

--- spotifywiki/app.js
+++ spotifywiki/app.js
@@ -25,7 +25,7 @@ app.use('/users', usersRouter);
 app.use('/loggedin', loginRouter);
 app.use('/no-song', indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 
 app.use(function(req, res, next) {
   next(createError(404));
 });
