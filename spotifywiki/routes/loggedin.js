fddffb1eb0c6c59950038da7b672f29967e5b9f9
bloomsa <bloomquist.sam@gmail.com>
2018-09-01 12:07:32 -0500

    simple implimentation/demo of how node-wikipedia will be used in project

--- spotifywiki/routes/loggedin.js
+++ spotifywiki/routes/loggedin.js
@@ -5,20 +5,17 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('client.properties');
+var wikipedia = require('node-wikipedia');
+var fastHTML = require('fast-html-parser');
 
 
 // i'm thinking all of the wikipedia queries will go in the 
 // '/' router get, and then the data can be passed along and rendered
 // in the login pug page
 
-// will most likely be using the get /page/html/{title} api endpoint to 
-// retreive the data from wikipedia and then have to check to make sure 
-// it's the song/album/artist we are looking for and then parse
-// to determine what information to display
 
 // function to convert the song title, artist, etc strings
-// obtained from spotify into the correct format for 
-// requesting through wikipedia rest api
+// obtained from spotify into the correct format 
 function stringToWikiFormat(s) {
   // works correctly, might still have some edge cases i haven't thought of
   s = s.charAt(0).toUpperCase() + s.substr(1); 
@@ -35,20 +32,27 @@ function stringToWikiFormat(s) {
 
 router.get('/', function (req, res, next) {
 
-  var wikirequest = {
-    url: 'https://en.wikipedia.org/api/rest_v1/page/html/' + stringToWikiFormat(req.query.song_name),
-    json: true
-  };
-  request.get(wikirequest).then(function(wikibody){
-    console.log(wikibody);
-  })
+  // var wikirequest = {
+  //   url: 'https://en.wikipedia.org/api/rest_v1/page/html/' + stringToWikiFormat(req.query.song_name),
+  //   json: true
+  // };
+  // request.get(wikirequest).then(function(wikibody){
+  //   var root = fastHTML.parse(wikibody);
+  //   console.log(root.querySelector('#1'));
+  // })
+
+  wikipedia.page.data("Diane_Young", { content: true }, function(response) {
+    // structured information on the page for Diane Young (wikilinks, references, categories, etc.)
+    // var root = fastHtml.parse(response.text);
+    // console.log(root.querySelector('#Personnel'));
+    console.log(response);
+  });
 
     res.render('login', { 
       name: stringToWikiFormat(req.query.user_name), 
       song_title: stringToWikiFormat(req.query.song_name), 
       artist: stringToWikiFormat(req.query.artist),
-      album: wikirequest
-      // album: stringToWikiFormat(req.query.album)
+      album: stringToWikiFormat(req.query.album)
     });  
   });
 
